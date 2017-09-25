bd9ab6568998294c2ba9da8f49e89e027c45e2ad
GODSKADI <agarcia-fraileperez@iesesteveterradas.cat>
2017-09-25 17:12:07 +0200

    Calendari html

---
+++ calendario_Adria_Garcia-Fraile.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html lang="es">
+
+<head>
+
+
+	<title>Intento de Calendario</title>
+
+	<meta charset="utf-8">
+
+	
+</head>
+
+<body>
+<h1>Intento de Calendario</h1>
+<table id="calendario">
+	<caption><?php echo $meses[$mes]." ".$año ?></caption>
+	<tr>
+		<th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th>
+		<th>Fri</th><th>Sat</th><th>Sun</th>
+	</tr>
+	<tr>
+		
+	</tr>
+</table>
